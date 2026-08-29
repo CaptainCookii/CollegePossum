7952625cd38d53d38d5d4351361412f4cf1afdc5
micwhee <micwhee@iu.edu>
2026-08-29 18:23:52 -0400

    outlines and yarn spinner fixes

--- CollegePossum/Assets/Scripts/YarnSpinnerHelpers/CharacterDialogue.cs
+++ CollegePossum/Assets/Scripts/YarnSpinnerHelpers/CharacterDialogue.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 using Yarn;
 using Yarn.Unity;
 
@@ -11,10 +12,21 @@ public class CharacterDialogue : MonoBehaviour
     public string dialogue1;
     public string dialogue2;
 
+    public GameObject outline;
+
     private bool interactable = true;
+    private int dialogue = 0;
+
+    private void Start()
+    {
+        // Make sure the outline starts invisible
+        outline.SetActive(false);
+    }
 
     private void Update()
     {
+        CheckMouseHover();
+
         if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame)
         {
             Vector2 mousePosition = Mouse.current.position.ReadValue();
@@ -26,23 +38,43 @@ public class CharacterDialogue : MonoBehaviour
 
             if (hit != null && hit.gameObject == gameObject && !dialogueRunner.IsDialogueRunning && interactable)
             {
-                interactable = false;
-                StartRandomDialogue();
+                StartDialogue();
             }
         }
     }
 
-    public void StartRandomDialogue()
+    private void CheckMouseHover()
     {
-        int randomDialogue = Random.Range(0, 2);
+        if (Mouse.current == null)
+            return;
 
-        if (randomDialogue == 0)
+        Vector2 mousePosition = Mouse.current.position.ReadValue();
+
+        Vector2 worldPosition =
+            Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Collider2D hit = Physics2D.OverlapPoint(worldPosition);
+
+        bool mouseOver = hit != null && hit.gameObject == gameObject;
+
+        outline.SetActive(false);
+        if (interactable && !dialogueRunner.IsDialogueRunning)
+        {
+            outline.SetActive(mouseOver);
+        }
+    }
+
+    public void StartDialogue()
+    {
+        if (dialogue == 0)
         {
             dialogueRunner.StartDialogue(dialogue1);
+            dialogue += 1;
         }
         else
         {
             dialogueRunner.StartDialogue(dialogue2);
+            interactable = false;
         }
     }
 }
