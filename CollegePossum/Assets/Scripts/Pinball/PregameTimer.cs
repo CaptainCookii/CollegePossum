eca76bb02fae7b822f346374bbcac1e6b06c0ed7
cjbytheocean <cjrodenbush2024@gmail.com>
2026-09-12 12:59:03 -0400

    add: pregame timer and related UI

--- CollegePossum/Assets/Scripts/Pinball/PregameTimer.cs
+++ CollegePossum/Assets/Scripts/Pinball/PregameTimer.cs
@@ -1,16 +1,37 @@
 using UnityEngine;
+using TMPro;
+using System.Collections;
+using Unity.VisualScripting;
 
 public class PregameTimer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] private TMP_Text instructions;
+    [SerializeField] private TMP_Text timer;
+
+    
     void Start()
     {
-        
+        instructions.gameObject.SetActive(false);
+        timer.gameObject.SetActive(false);
+        StartCoroutine(Sequence()); 
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator Sequence()
     {
-        
+        instructions.gameObject.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        instructions.gameObject.SetActive(false);
+
+        timer.gameObject.SetActive(true);
+
+        for (int i = 20; i > 0; i--)
+        {
+            timer.text = "Balls Drop In: " + i + "s";
+            yield return new WaitForSeconds(1f);
+        }
+
+        timer.gameObject.SetActive(false);
+
     }
 }
