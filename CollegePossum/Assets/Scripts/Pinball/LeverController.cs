695bb44c48ea0f9c4ed212fe7dbf6aedc5e7ef47
cjbytheocean <cjrodenbush2024@gmail.com>
2026-09-14 21:30:00 -0400

    add: LeverController.cs documentation

--- CollegePossum/Assets/Scripts/Pinball/LeverController.cs
+++ CollegePossum/Assets/Scripts/Pinball/LeverController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class LeverController : MonoBehaviour
 {
@@ -15,21 +16,58 @@ public class LeverController : MonoBehaviour
     private HingeJoint2D hinge;
     private JointMotor2D motor;
 
+
+    // Stores hinge component and attacks motor to hinge.
     private void Awake()
     {
         hinge = GetComponent<HingeJoint2D>();
         motor = hinge.motor;
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    // Moves the levers their respective directions with their respective forces.
+    // Angles are edited in Unity engine.
     void Update()
     {
-        
+        bool keyPressed = false;
+
+        if (side == Side.Left)
+        {
+            if (Keyboard.current.leftArrowKey.isPressed)
+            {
+                keyPressed = true;
+            }
+        }
+        else if (side == Side.Right)
+        {
+            if (Keyboard.current.rightArrowKey.isPressed)
+            {
+                keyPressed = true;
+            }
+        }
+
+        if (keyPressed)
+        {
+            if (side == Side.Left)
+            {
+                motor.motorSpeed = force;
+            }
+            else
+            {
+                motor.motorSpeed = -force;
+            }
+        }
+        else
+        {
+            if (side == Side.Left)
+            {
+                motor.motorSpeed = -stoppedForce;
+            }
+            else
+            {
+                motor.motorSpeed = stoppedForce;
+            } 
+        }
+
+        hinge.motor = motor;  
     }
 }
